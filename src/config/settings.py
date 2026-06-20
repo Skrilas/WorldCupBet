7ffd78c07c737d42326bb21bbaeb3074d939225b
Skrilas <2023326465@aluno.osorio.ifrs.edu.br>
2026-06-20 01:49:53 -0300

    renomeação da api_time_url da .env para api_url para uso genérico

--- src/config/settings.py
+++ src/config/settings.py
@@ -3,7 +3,7 @@ from pydantic import Field
 
 class Settings(BaseSettings):
     database_url: str = Field(alias="DATABASE_URL")
-    api_jogos_url: str = Field(alias="API_JOGOS_URL")
+    api_url: str = Field(alias="API_URL")
 
     model_config = SettingsConfigDict(env_file=".env")
 
