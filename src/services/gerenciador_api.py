f5da54a810546579e7b09338d1af0145c3efee93
TIAGO MAIER FERREIRA CARDOSO <2023326465@ALUNOS.OSORIO.IFRS.EDU.BR>
2026-06-23 11:50:35 -0300

    criado uma função genérica pra configurar a url da api

---
+++ src/services/gerenciador_api.py
@@ -0,0 +1,24 @@
+from config.settings import settings
+import requests
+
+class GerenciadorApi:
+    headers = { #simular um navegador pra obter dados da api
+            "User-Agent": (
+            "Mozilla/5.0 (Windows NT 10.0; Win64; x64) "
+            "AppleWebKit/537.36 (KHTML, like Gecko) "
+            "Chrome/137.0 Safari/537.36"
+            )}
+    @classmethod
+    def obter_dados(cls, tipo: str):
+        
+        endpoint = f"{settings.api_url}{tipo}" #configura url pra receber o json das partidas
+        try:
+            response = requests.get(endpoint, headers=cls.headers, timeout=20)
+            response.raise_for_status()
+        except requests.RequestException as e:
+            raise Exception(f"Erro ao acessar a API: {e}") from e
+        
+        dados = response.json()
+        if tipo not in dados:
+            raise Exception(f"A resposta da API não contém a chave '{tipo}'")
+        return dados[tipo]
--- src/services/time_service.py
+++ src/services/time_service.py
@@ -5,35 +5,13 @@ import requests
 from models.time import Time
 from repository.time_repository import TimeRepository
 from schemas.api_time import ApiTime
-from config.settings import settings
+from services.gerenciador_api import GerenciadorApi
 
 class TimeService:
-    @classmethod
-    def obter_dados(cls):
-        headers = {
-            "User-Agent": (
-            "Mozilla/5.0 (Windows NT 10.0; Win64; x64) "
-            "AppleWebKit/537.36 (KHTML, like Gecko) "
-            "Chrome/137.0 Safari/537.36"
-            )}
-        endpoint = f"{settings.api_url}teams" #configura url pra receber o json dos times
-        try:
-            response = requests.get(endpoint, headers=headers, timeout=20)
-            response.raise_for_status()
-            
-        except requests.RequestException as e:
-            raise Exception(f"Erro ao acessar a API: {e}")
-        
-        dados = response.json()
-        if "teams" not in dados:
-            raise Exception("A resposta da API não contém a chave 'teams'.")
-
-        return dados["teams"]
-            
     
-    @classmethod
-    def criar_times(cls):
-        times = cls.obter_dados()
+    @staticmethod
+    def criar_times():
+        times = GerenciadorApi.obter_dados("teams")
         with Session(engine) as session:
             repo = TimeRepository(session)
 
