ed3dc4fd6e33ba4d458d47a91b5e4aaea8dcc105
Skrilas <2023326465@aluno.osorio.ifrs.edu.br>
2026-07-23 12:22:08 -0300

    test: test de funcionamento da sincornização das partidas com o banco

--- src/time_seed.py
+++ src/time_seed.py
@@ -1,5 +1,6 @@
 from repository.apostas_repository import ApostasRepository
 from services.gerenciador_api_historico import GerenciadoApiHistorico
+from services.sync_partidas_service import SyncPartidasService
 from services.time_service import TimeService
 from sqlmodel import Session
 from database import engine
@@ -9,6 +10,7 @@ if __name__ == "__main__":
         # repo = ApostasRepository(session)
         # resultado = repo.obter_estatisticas_aposta(2)
         # print(resultado)
-    time = TimeService.buscar_historico_copas(9)
+    # time = TimeService.buscar_historico_copas(9)
 
-    print(time)
+    # print(time)
+    SyncPartidasService.sincronizar_partidas()
