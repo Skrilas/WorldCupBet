348740e009a33e7c28fd325d5333ff695c1e096c
Skrilas <2023326465@aluno.osorio.ifrs.edu.br>
2026-07-23 12:18:46 -0300

    fix: validator criado pra funcionar a validação do pydantic

--- src/schemas/api_partida.py
+++ src/schemas/api_partida.py
@@ -1,10 +1,16 @@
-from pydantic import BaseModel
+from pydantic import BaseModel, Field, field_validator
 from datetime import datetime
 
 class ApiPartida(BaseModel):
+    api_id: int = Field(validation_alias="id")
     home_team_id: int
     away_team_id: int
-    home_scorers: int
-    away_scorers: int
+    home_score: int
+    away_score: int
     local_date: datetime
     finished: bool
+
+    @field_validator("local_date", mode="before")
+    @classmethod
+    def converter_data(cls, value: str) -> datetime:
+        return datetime.strptime(value, "%m/%d/%Y %H:%M")
