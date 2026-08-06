5f922afd2b7cb22e0bb16281eb8f9339432dd2b9
Tiago Maier Ferreira Cardoso <2023326465@aluno.osorio.ifrs.edu.br>
2026-08-06 11:28:00 -0300

    build: troca de API da copa

--- src/config/settings.py
+++ src/config/settings.py
@@ -4,6 +4,7 @@ from pydantic import Field
 class Settings(BaseSettings):
     database_url: str = Field(alias="DATABASE_URL")
     api_url: str = Field(alias="API_URL")
+    api_token: str = Field(alias="API_TOKEN")
     historico_api_url: str = Field(alias="HISTORICO_API_URL")
     historico_api_key: str = Field(alias="HISTORICO_API_KEY")
 
